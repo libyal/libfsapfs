5142e8ad0510fbf25e84bfad5eac19de2bb93373
Joachim Metz <joachim.metz@gmail.com>
2018-09-11 08:58:03 +0200

    Changes for deployment

--- syncsharedlibs.sh
+++ syncsharedlibs.sh
@@ -7,7 +7,7 @@ EXIT_SUCCESS=0;
 EXIT_FAILURE=1;
 
 GIT_URL_PREFIX="https://github.com/libyal";
-SHARED_LIBS="libcerror libcthreads libcdata libclocale libcnotify libcsplit libuna libcfile libcpath libbfio libfdatetime";
+SHARED_LIBS="libcerror libcthreads libcdata libclocale libcnotify libcsplit libuna libcfile libcpath libbfio libfdatetime libfguid";
 
 if test "${TRAVIS_OS_NAME}" != "linux";
 then
